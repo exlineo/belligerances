dfe29896b48581ffe5a44554aa8af406dc6f3d49
FabriqueNumerique <fabriquenumeriquepaloise@gmail.com>
2024-03-03 20:10:57 +0100

    Correction d'un primeng trainant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UtilsService } from './shared/services/utils.service';
-import { PrimeNGConfig, FilterMatchMode } from 'primeng/api';
 import { DonneesService } from './shared/services/donnees.service';
 
 @Component({
@@ -11,23 +10,13 @@ import { DonneesService } from './shared/services/donnees.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   title = 'Belligerance';
   l: UtilsService = inject(UtilsService); // Langues et autres infos
   d:DonneesService = inject(DonneesService); // Les données du logiciel
 
-  constructor(private primeConfig: PrimeNGConfig) {
+  constructor() {
     this.l.getLangue();
-    console.log(this.l.t);
-  }
-  // Configurations et chargement de données
-  ngOnInit(): void {
-    this.primeConfig.ripple = true;
-    this.primeConfig.filterMatchModeOptions = {
-      text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
-      numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
-      date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
-    };
   }
 }
